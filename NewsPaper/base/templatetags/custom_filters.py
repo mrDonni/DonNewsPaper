09a83ceb51694f95a0147c254c09be8a34d1b95c
mrDonni <75480310+mrDonni@users.noreply.github.com>
2021-02-18 03:22:33 +0300

    h

---
+++ NewsPaper/base/templatetags/custom_filters.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()  # если мы не зарегестрируем наши фильтры,
+# то django никогда не узнает где именно их искать и фильтры потеряются(
+
+@register.filter(name = 'strong_language')
+def strong_language(value):
+    words = ['пиздец','Хуй','Жопа','Нигер']
+    for x in words:
+        x = str(x)
+        if x.lower()  in str(value.lower()):
+           value = value.replace(x.lower(),"***").replace(x.title(),'***')
+    return value
